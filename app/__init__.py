0e7a8890c3dfb3da7129546a91d36a3bef50582b
Ben Vincent <vincent@ualberta.ca>
2016-04-19 12:50:49 -0600

    Added markdown, added info to index.html

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.autodoc import Autodoc
+from flask.ext.misaka import Misaka
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -13,7 +14,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = "False"
 app.config["PORT"] = os.environ.get("PORT", "5000")
 
 db = SQLAlchemy(app)
-
 auto = Autodoc(app)
+md = Misaka()
+md.init_app(app)
 
 from app import views, models
