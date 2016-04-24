ec554dea202f1b5055444e686d2c3ee7325fe282
Ben Vincent <vincent@ualberta.ca>
2016-04-24 16:05:28 -0600

    prettied up index.html & documentation.html

--- app/views.py
+++ app/views.py
@@ -4,9 +4,20 @@ from app import *
 from app import api
 from flask import render_template
 
+import requests
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    raw_feed = requests.get("https://api.github.com/users/bvincent1/events/public").json()
+    git_feed = []
+
+    for event in raw_feed:
+        if event["type"] == "PushEvent" and event["repo"]["name"] == "bvincent1/institutional":
+            for commit in event["payload"]["commits"]:
+                git_feed.append({ "message": commit["message"] })
+
+
+    return render_template('index.html', git=git_feed[:5])
 
 
 @app.route('/robots.txt')
